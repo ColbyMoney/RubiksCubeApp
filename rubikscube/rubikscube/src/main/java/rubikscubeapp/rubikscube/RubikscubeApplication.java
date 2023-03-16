ffdcf6e26ba4f346ba4f6bbcb8c4788b20b3e608
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-03-15 22:16:34 -0500

    Create Spring Boot project

---
+++ rubikscube/rubikscube/src/main/java/rubikscubeapp/rubikscube/RubikscubeApplication.java
@@ -0,0 +1,13 @@
+package rubikscubeapp.rubikscube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubikscubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubikscubeApplication.class, args);
+	}
+
+}
---
+++ rubikscube/rubikscube/src/test/java/rubikscubeapp/rubikscube/RubikscubeApplicationTests.java
@@ -0,0 +1,13 @@
+package rubikscubeapp.rubikscube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubikscubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
