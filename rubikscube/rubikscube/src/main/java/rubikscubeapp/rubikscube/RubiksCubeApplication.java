0cc93a6c709fd269d02cb5c7cf7189621f4a430e
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-03-17 10:38:57 -0500

    fix backend dependencies

--- rubikscube/rubikscube/src/main/java/rubikscubeapp/rubikscube/RubiksCubeApplication.java
+++ rubikscube/rubikscube/src/main/java/rubikscubeapp/rubikscube/RubiksCubeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RubikscubeApplication {
+public class RubiksCubeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RubikscubeApplication.class, args);
+		SpringApplication.run(RubiksCubeApplication.class, args);
 	}
 
 }
